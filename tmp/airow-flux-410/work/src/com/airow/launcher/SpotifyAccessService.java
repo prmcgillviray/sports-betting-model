e5426dc9c7ed574a446805b6e2d76205b0a3b9d6
prmcgillviray <prmcgillviray@gmail.com>
2026-09-22 08:53:55 -0400

    Stage AIROW Flux src/com/airow/launcher/SpotifyAccessService.java

---
+++ tmp/airow-flux-410/work/src/com/airow/launcher/SpotifyAccessService.java
@@ -0,0 +1,9 @@
+package com.airow.launcher;
+
+import android.service.notification.NotificationListenerService;
+
+/**
+ * Grants access to Android media sessions after the user enables this service. No notification
+ * contents are read, stored, or transmitted.
+ */
+public final class SpotifyAccessService extends NotificationListenerService {}
